c5b6b69c99e28973017f053973ed75ef71e76033
wongro2 <roy.wong@oocl.com>
2020-04-08 18:40:50 +0800

    [Roy] add small logic to calculateResult to fulfill testcase1A0B

--- src/main/java/com/oocl/GuessNumber.java
+++ src/main/java/com/oocl/GuessNumber.java
@@ -1,6 +1,20 @@
 package com.oocl;
 
 public class GuessNumber {
-    public String calculateResult(String s) {
+    public String calculateResult(String inputNumber) {
+        String expectedNumber = "1234";
+        String returnString="";
+        int valueA = 0;
+        int valueB = 0;
+
+        for (int index = 0; index<inputNumber.length(); index ++ ){
+            if (inputNumber.charAt(index)==expectedNumber.charAt(index)){
+                valueA ++;
+                return String.format("%dA%dB",valueA,valueB);
+            }
+        }
+        return returnString;
+
+
     }
 }
