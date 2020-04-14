cf522f3307cb817b1f9a172b79aeede5dfaa87ed
wongro2 <roy.wong@oocl.com>
2020-04-14 20:40:53 +0800

    [Roy] refactor game logic to App Class

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,13 +1,34 @@
 package com.oocl;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static final String WRONG_INPUT_MESSAGE = "Wrong Input, Input again!";
+    public static final String CORRECT_OUTPUT = "4A0B";
+    public static final String WIN_MESSAGE = "\nYou win!";
+
+    public static void main(String[] args) {
+        GuessNumber game = new GuessNumber();
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+        InputReader inputReader = new InputReader();
+        String expectedResult = answerGenerator.generateRandomNumber();
+        String calculatedResult = "";
+
+        int gameCounter = 1;
+        while (GuessNumber.isGameOn(gameCounter)) {
+            String userInput = inputReader.getUserInput();
+            while (!GuessNumber.isInputValid(game, userInput) && GuessNumber.isGameOn(gameCounter)) {
+                System.out.println(WRONG_INPUT_MESSAGE);
+                userInput = inputReader.getUserInput();
+                gameCounter++;
+            }
+            if (GuessNumber.isInputValid(game, userInput) && GuessNumber.isGameOn(gameCounter)) {
+                calculatedResult = game.calculateResult(userInput, expectedResult);
+                System.out.print(calculatedResult);
+                if (calculatedResult.equals(CORRECT_OUTPUT)) {
+                    System.out.print(WIN_MESSAGE);
+                    break;
+                }
+                gameCounter++;
+            }
+        }
     }
 }
--- src/main/java/com/oocl/GuessNumber.java
+++ src/main/java/com/oocl/GuessNumber.java
@@ -4,44 +4,16 @@ import java.util.*;
 
 public class GuessNumber {
     public static final int MAX_GAME_PLAY = 6;
-    public static final String WRONG_INPUT_MESSAGE = "Wrong Input, Input again!";
-    public static final String CORRECT_OUTPUT = "4A0B";
-    public static final String WIN_MESSAGE = "\nYou win!";
+    public static final String DELIMITER = "";
+    public static final int MAX_LENGTH_DIGIT = 4;
 
-    public static void main(String[] args) {
-        GuessNumber game = new GuessNumber();
-        AnswerGenerator answerGenerator = new AnswerGenerator();
-        InputReader inputReader = new InputReader();
-        String expectedResult = answerGenerator.generateRandomNumber();
-        String calculatedResult = "";
-        int gameCounter = 1;
-        while (isGameOn(gameCounter)) {
-            String userInput = inputReader.getUserInput();
-            while (!isInputValid(game, userInput) && isGameOn(gameCounter)) {
-                System.out.println(WRONG_INPUT_MESSAGE);
-                userInput = inputReader.getUserInput();
-                gameCounter++;
-            }
-            if (isInputValid(game, userInput) && isGameOn(gameCounter)) {
-                calculatedResult = game.calculateResult(userInput, expectedResult);
-                System.out.print(calculatedResult);
-                if (calculatedResult.equals(CORRECT_OUTPUT)) {
-                    System.out.print(WIN_MESSAGE);
-                    break;
-                }
-                gameCounter++;
-            }
-        }
-    }
-
-    private static boolean isInputValid(GuessNumber game, String userInput) {
+    public static boolean isInputValid(GuessNumber game, String userInput) {
         return game.checkUserInput(userInput);
     }
 
-    private static boolean isGameOn(int gameCounter) {
+    public static boolean isGameOn(int gameCounter) {
         return gameCounter <= MAX_GAME_PLAY;
     }
-
 
     public String calculateResult(String inputNumber, String expectedNumber) {
         int valueA = 0;
@@ -60,13 +32,11 @@ public class GuessNumber {
     }
 
     public boolean checkUserInput(String userInput) {
-        int requiredLengthOfInput = 4;
         List<String> checkDistinct;
-        if (userInput.length() != requiredLengthOfInput) {
-            //System.out.println(userInput.length());
+        if (userInput.length() != MAX_LENGTH_DIGIT) {
             return false;
         }
-        checkDistinct = Arrays.asList(userInput.split(""));
+        checkDistinct = Arrays.asList(userInput.split(DELIMITER));
         if (checkDistinct.size() != checkDistinct.stream().distinct().count()) {
             return false;
         }
