312a7bd614b5f58baa196bca5922503321592898
wongro2 <roy.wong@oocl.com>
2020-04-14 20:15:50 +0800

    [Roy] removed zombie code and changed magic number

--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -1,53 +1,58 @@
 package com.oocl;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class GuessNumberTest{
+    public static final String EXPECTED_NUMBER = "1234";
+
     @Test
     public void should_output_1A0B (){
         GuessNumber guessnumber = new GuessNumber();
-        String result = guessnumber.calculateResult("1567","1234");
+        String result = guessnumber.calculateResult("1567", EXPECTED_NUMBER);
         assertEquals("1A0B",result);
     }
     @Test
     public void should_output_0A2B (){
         GuessNumber guessnumber = new GuessNumber();
-        String result = guessnumber.calculateResult("2478","1234");
+        String result = guessnumber.calculateResult("2478", EXPECTED_NUMBER);
         assertEquals("0A2B",result);
     }
     @Test
     public void should_output_1A2B (){
         GuessNumber guessnumber = new GuessNumber();
-        String result = guessnumber.calculateResult("0324","1234");
+        String result = guessnumber.calculateResult("0324", EXPECTED_NUMBER);
         assertEquals("1A2B",result);
     }
     @Test
     public void should_output_0A0B (){
         GuessNumber guessnumber = new GuessNumber();
-        String result = guessnumber.calculateResult("5678","1234");
+        String result = guessnumber.calculateResult("5678", EXPECTED_NUMBER);
         assertEquals("0A0B",result);
     }
     @Test
     public void should_output_0A4B (){
         GuessNumber guessnumber = new GuessNumber();
-        String result = guessnumber.calculateResult("4321","1234");
+        String result = guessnumber.calculateResult("4321", EXPECTED_NUMBER);
         assertEquals("0A4B",result);
     }
     @Test
     public void should_output_4A0B (){
         GuessNumber guessnumber = new GuessNumber();
-        String result = guessnumber.calculateResult("1234","1234");
+        String result = guessnumber.calculateResult(EXPECTED_NUMBER, EXPECTED_NUMBER);
         assertEquals("4A0B",result);
     }
 
-    /*@Test
+    @Test
     public void should_generate_some_number(){
-        GuessNumber guessnumber = new GuessNumber();
         AnswerGenerator answerGenerator = new AnswerGenerator();
         String randomNumber = answerGenerator.generateRandomNumber();
         List<String> checkNumberList;
@@ -57,11 +62,10 @@ public class GuessNumberTest{
             nonRepeat = true;
         }
         Assert.assertEquals(true, nonRepeat);
-    }*/
+    }
 
     @Test
     public void should_read_input_from_user(){
-        //GuessNumber guessnumber = new GuessNumber();
         InputReader inputReader = new InputReader();
         String SimulatedInput = "1379";
         InputStream in = new ByteArrayInputStream(SimulatedInput.getBytes());
